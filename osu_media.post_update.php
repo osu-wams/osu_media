94e2f695a0c3af9053c9fc58c88421a319c4bd98
Matthew Brabham <brabham.matthew@gmail.com>
2022-07-22 10:56:20 -0700

    Add frameless as a video embed option.

--- osu_media.post_update.php
+++ osu_media.post_update.php
@@ -5,6 +5,8 @@
  * Post Update file.
  */
 
+use Drupal\Core\Config\FileStorage;
+
 /**
  * Remove view mode for media_card_caption.
  */
@@ -21,4 +23,20 @@ function osu_media_post_update_remove_card_caption_display(&$sandbox) {
   $media_card_caption = \Drupal::service('config.factory')
     ->getEditable('core.entity_view_mode.media.media_card_caption');
   $media_card_caption->delete();
+}
+
+/**
+ * Add Frameless as an option for remote video.
+ */
+function osu_media_post_update_add_frameless_embed_option(&$sandbox) {
+  $config_name = 'core.entity_view_display.media.remote_video.frameless';
+  /** @var string $osu_media_module_path */
+  $osu_media_module_path = \Drupal::service('module_handler')
+    ->getModule('osu_media')
+    ->getPath();
+  $config_path = realpath($osu_media_module_path . '/config/install');
+  /** @var \Drupal\Core\Config\StorageInterface $config_storage */
+  $config_storage = \Drupal::service('config.storage');
+  $config_source = new FileStorage($config_path);
+  $config_storage->write($config_name, $config_source->read($config_name));
 }
